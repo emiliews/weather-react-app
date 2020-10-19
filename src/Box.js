b9f2fef0dc30d2e7ee3eb634452ca71f6f209889
emiliews <emilie@wolff-sneedorff.dk>
2020-10-19 16:16:29 +0200

    Editing card text

--- src/Box.js
+++ src/Box.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Box.css";
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
+import { Col } from "react-bootstrap";
 
 export default function Box() {
   let weatherData = {
@@ -18,11 +19,47 @@ export default function Box() {
   
   return (
     <div className="Box">
-      <Card style={{ width: '18rem' }}>
+        <h1 id="city-header">
+            {weatherData.city}
+        </h1>
+        <hr />
+      <Card style={{ width: '28rem' }}>
         <Card.Body>
-            <Card.Title id="city-header">{weatherData.city}</Card.Title>
+            <Card.Title id="day"> Today </Card.Title>
+            <br/>
+            <Card.Title id="time">    {" "}
+            Now{" "} </Card.Title>
+           
             <Card.Text>
-            Some quick example text to build on the card title and make up the bulk of the card's content.
+                <span id="temp"> {weatherData.temperature} </span>
+                <span className="tempUnit" id="tempUnit">
+                    <a href="/" id="celsius-link" className="active temp">
+                        °C
+                    </a>{" "}
+                    |{" "}
+                    <a href="/" id="fahrenheit-link" className="temp">
+                        °F
+                    </a>
+                </span> 
+                &#8287;&#8287;&#8287;&#8287;{" "}
+                <span>
+                    <img
+                        src={weatherData.imgUrl}
+                        alt={weatherData.description}
+                        style={{ width: 60, height: 60 }}
+                    />
+                </span> 
+                <Row id="tempwindhum">
+                    <Col>
+                        <strong> Temperatures </strong>
+                    </Col>
+                    <Col>
+                    
+                    </Col>
+                    <Col>
+                    
+                    </Col>
+                </Row>          
             </Card.Text>
             <Button variant="primary">Go somewhere</Button>
         </Card.Body>
