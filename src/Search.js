a7319aea44082a2634e13843ed79a43bb848ca32
emiliews <emilie@wolff-sneedorff.dk>
2020-10-20 15:27:19 +0200

    Deleted unneccessary code and files

--- src/Search.js
+++ src/Search.js
@@ -6,9 +6,11 @@ export default function Search() {
     <div className="Search">
       <form className="search-form" id="search-form">
         <input type="search" placeholder="  Enter city" id="city-input" />
+        {" "}
         <button className="btn btn-primary" type="submit" id="searchButton">
           Search
         </button>
+        {" "}
         <button className="btn btn-primary" type="submit" id="currentButton">
           Current city
         </button>
